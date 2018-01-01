cacc79a95ef4a69e818ebdd3aace803cb9d0bf5b
Yukino Song <syqlds@126.com>
2018-01-01 13:28:32 +0800

    Upgrade dependencies

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const parse = require('eft-parser')
 
 module.exports = function(template) {
 	if (this.cacheable) this.cacheable()
-	return `const {create} = require('ef-core'); module.exports = create(${JSON.stringify(parse(template))})`
+	return `module.exports = require('ef-core').create(${JSON.stringify(parse(template))})`
 }
