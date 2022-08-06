5e38718809b21ca4705e00ebfd3eefdaa4e61324
Yukino Song <yukino@yuki.network>
2022-08-07 00:12:15 +0800

    Catch up with rollup plugin

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-const parse = require('eft-parser')
+import path from 'path'
+import parse from 'eft-parser'
+import camelCase from 'camelcase'
 
-module.exports = function(template) {
+export default function(template) {
 	if (this.cacheable) this.cacheable()
-	return `module.exports = require('ef-core').create(${JSON.stringify(parse(template))})`
+
+	const filePath = this.resourcePath
+	const fileName = path.parse(filePath).name
+
+	const importLines = []
+	const exportLines = []
+	let componentName = camelCase(fileName, {pascalCase: true})
+	let componentScope = null
+
+	const commentHandler = ({depth, content}) => {
+		if (depth > 0) return
+		if (content[0] !== '!') return
+
+		content = content.slice(1).trim()
+
+		const splitedContent = content.split(/\s+/)
+		const directive = splitedContent.shift()
+
+		switch (directive) {
+			case 'import': {
+				importLines.push(content)
+				break
+			}
+			case 'export': {
+				exportLines.push(content)
+				break
+			}
+			case 'scope': {
+				componentScope = content.match(/{.+}/)
+				break
+			}
+			case 'name': {
+				componentName = camelCase(splitedContent.join('_'), {pascalCase: true})
+				break
+			}
+			default: {
+				throw new TypeError(`[EFML] Unknown directive "${directive}"`)
+			}
+		}
+
+	}
+
+	const ast = JSON.stringify(JSON.stringify(parse(template, commentHandler)))
+
+	const code = [
+		...importLines,
+		componentScope &&
+			`import { create, scoped } from 'ef-core'` ||
+			`import { create } from 'ef-core'`,
+		componentScope &&
+			`export default class ${componentName} extends scoped(create(JSON.parse(${ast})), ${componentScope}) {}` ||
+			`export default class ${componentName} extends create(JSON.parse(${ast})) {}`,
+		...exportLines,
+		''
+	]
+
+	return code.join(';\n')
 }
